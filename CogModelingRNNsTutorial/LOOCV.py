1007e3da1ecdd3c807f1d1773c03e49e25bc2e3b
Meike Oschmann <meikeoschmann@gmail.com>
2024-01-10 13:05:29 +0100

    save model outputs for plotting

--- CogModelingRNNsTutorial/LOOCV.py
+++ CogModelingRNNsTutorial/LOOCV.py
@@ -4,79 +4,106 @@ import jax.numpy as jnp
 import numpy as np
 import optax
 import pandas as pd
+import os
 from scipy.io import loadmat
+import pickle
 import disrnn
 import rnn_utils
 from rnn_utils import DatasetRNN
 
+
+# Load data
 matDf = loadmat('data\data_for_Meike.mat')['data']
 features = matDf.dtype.names
 df = pd.DataFrame(np.squeeze(np.array(matDf.tolist())).T, columns=features).drop(columns=['label','Spcorrect1', 'Spcorrect2', 'Snoise']).sort_values(by=['subject', 'trial'])
 
-# for now only partner 1
-df = df[df['type'] == 1]
+# Filter out broken subjects
+subject_list = [0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 12, 13, 14, 15, 18, 20, 21, 24, 25, 26, 27, 28, 29]
+df = df[df['subject'].isin(subject_list)]
 
 df['Reward'] = (df['select'] == 1) * df['Sacc'] + (df['select'] == -1) * df['Pacc']
 df['theta'] = np.absolute(df['dtheta'])
 df['theta_next'] = df['theta'].shift(periods = 1, fill_value = 0)
 df['Sacc_next'] = df['Sacc'].shift(periods = 1, fill_value = 0)
 
-# features = ['block', 'theta', 'theta_next', 'Preport', 'Pacc', 'Schoice', 'Sacc', 'Sacc_next', 'select', 'Reward', 'Sreport'] 
-features = ['theta', 'Preport', 'Pacc', 'Schoice', 'Sacc', 'select', 'Reward', 'Sreport'] 
+# Create Sconfidence for each subject and each block, relative to mean of Sreport: 1 = Sreport > mean, 0 = Sreport < mean
+df['Sconfidence'] = df.groupby(['subject', 'block'])['Sreport'].transform(lambda x: np.where(x >= np.mean(x), 1, 0))
+df['Pconfidence'] = df.groupby(['subject', 'block'])['Preport'].transform(lambda x: np.where(x >= np.mean(x), 1, 0))
 
-target = ['Sreport']
+features = ['block', 'type', 'theta', 'Pconfidence', 'Pacc', 'Schoice', 'Sacc', 'select', 'Reward', 'Sconfidence'] 
 
+# Create target: for a given index in df, find the next index that has the same value for df['type'] and add df['Sconfidence'] of that index to the target
+target = []
+# assert that target has the same length as df, i.e. that i == len(target)
+
+for i in range(len(df)):
+    for j in range(i+1, len(df)):
+        if df['subject'].iloc[i] == df['subject'].iloc[j] and df['block'].iloc[i] == df['block'].iloc[j] and df['type'].iloc[i] == df['type'].iloc[j]:
+            target.append(df['Sconfidence'].iloc[j])
+            break
+
+        # if the next trial is the first one of the next block, append -1
+        elif df['subject'].iloc[i] == df['subject'].iloc[j] and (df['block'].iloc[i] + 1) == df['block'].iloc[j]:
+            target.append(-1)
+            break
+
+        # if the next trial is the first one of the next subject, append -1
+        elif (subject_list.index(df['subject'].iloc[i])+1 < len(subject_list)) and (subject_list[subject_list.index(df['subject'].iloc[i])+1]) == df['subject'].iloc[j] and df['block'].iloc[i] == 4 and df['block'].iloc[j] == 1:
+            target.append(-1)
+            break
+# no target for the last two trials, append -1 twice    
+target.append(-1)
+target.append(-1)
 
 def main():
     likelihoods = np.zeros((30,2))
-    for i in range(df['subject'].unique().size):
+    #for i in range(df['subject'].unique().size): 
+    for i in subject_list: # error for subject 9, 11, 18, 20, 23, 24; LL = 0% for subject 10, 22, 26, 30
         train_LL, test_LL = train_network_LOOCV(i)
         likelihoods[i,:] = [train_LL, test_LL]
     print(likelihoods)
     np.savetxt("likelihoods.csv", likelihoods, delimiter=",")
 
 def train_network_LOOCV(leave_out_idx):
-    print(f'Leave out subject: {leave_out_idx+1}')
-    batch_size = None
-    target_size = df['Sreport'].unique().size
-    n_sessions = df['subject'].unique().size
-    n_trials = int(len(df) / n_sessions)
+    print(f'Leave out subject: {subject_list[leave_out_idx]+1}')
+    target_size = df['Sconfidence'].unique().size
+    n_subjects = df['subject'].unique().size
+    n_trials = int(len(df) / n_subjects)
     n_blocks = df['block'].unique().size
-    features_number = len(features)
-
-    xsTrain = np.zeros((n_trials, n_sessions, features_number))
-    ysTrain = np.zeros((n_trials, n_sessions, 1))
-    xsTest = np.zeros((n_trials, 1, features_number))
+    n_features = len(features)
+    batch_size = None
+    
+    xsTrain = np.zeros((n_trials, n_subjects, n_features))
+    ysTrain = np.zeros((n_trials, n_subjects, 1))
+    xsTest = np.zeros((n_trials, 1, n_features))
     ysTest = np.zeros((n_trials, 1, 1))
 
     for i, subject in enumerate(np.sort(df['subject'].unique())):
         xsTrain[:, i, :] = df[df['subject'] == subject][features].values
-        ysTrain[:, i, :] = df[df['subject'] == subject][target].values
-        xsTest[:, 0, :] = df[df['subject'] == subject][features].values[leave_out_idx,:]
-        ysTest[:, 0, :] = df[df['subject'] == subject][target].values[leave_out_idx,:]
-            
+        
+    # reshape target to fit the shape of ysTrain
+    ysTrain = np.array(target).reshape(n_trials, n_subjects, 1, order='F')
+    
+    # The test set consists of the subject that is left out
+    xsTest[:, 0, :] = xsTrain[:, leave_out_idx, :]
+    ysTest[:, 0, :] = ysTrain[:, leave_out_idx, :]    
+        
     # Exclude leave_out_idx from xsTrain and ysTrain
     xsTrain = np.delete(xsTrain, leave_out_idx, axis=1)
     ysTrain = np.delete(ysTrain, leave_out_idx, axis=1)
 
-    n_sessions -= 1
+    n_subjects -= 1
 
-    border = int(n_trials / len(df['block'].unique()))
-
-    indices_xsTrain = np.arange(0, n_blocks * border, border)
-    indices_ysTrain = np.arange(border, n_blocks * border, border)
+    # division between blocks
+    border = int(n_trials / len(df['block'].unique())) # number of trials per block
+    indices = np.arange(border, n_blocks * border, border)
     
-    # Add a dummy input at the beginning of each block. First step has a target but no input
-    xsTrain_padded_LOOCV = np.insert(xsTrain, indices_xsTrain, np.zeros((1, n_sessions, features_number)), axis=0)
-    xsTest_padded_LOOCV = np.insert(xsTest, indices_xsTrain, np.zeros((1, 1, features_number)), axis=0)
-
-    # Add a dummy target at the end of each block. Last step has input but no target
-    ysTrain_padded_LOOCV  = np.insert(ysTrain, indices_ysTrain, -1*np.ones((1, n_sessions, 1)), axis=0)
-    ysTest_padded_LOOCV = np.insert(ysTest, indices_ysTrain, -1*np.ones((1, 1, 1)), axis=0)
-
-    # np.insert inserts before given idx, so the last row we need to append manually
-    ysTrain_padded_LOOCV = np.concatenate((ysTrain_padded_LOOCV, -1*np.ones((1,n_sessions,1))), axis=0)
-    ysTest_padded_LOOCV = np.concatenate((ysTest_padded_LOOCV, -1*np.ones((1,1,1))), axis=0)
+    # Add a dummy between the blocks
+    xsTrain_padded_LOOCV = np.insert(xsTrain, indices, np.zeros((1, n_subjects, n_features)), axis=0)
+    xsTest_padded_LOOCV = np.insert(xsTest, indices, np.zeros((1, 1, n_features)), axis=0)
+    # The targets already contain -1s, but we need to add one more to seperate the blocks
+    ysTrain_padded_LOOCV  = np.insert(ysTrain, indices, -1*np.ones((1, n_subjects, 1)), axis=0)
+    ysTest_padded_LOOCV = np.insert(ysTest, indices, -1*np.ones((1, 1, 1)), axis=0)
 
     train = DatasetRNN(xsTrain_padded_LOOCV, ysTrain_padded_LOOCV, batch_size)
     test = DatasetRNN(xsTest_padded_LOOCV, ysTest_padded_LOOCV, batch_size)
@@ -84,13 +111,8 @@ def train_network_LOOCV(leave_out_idx):
     # Set up the DisRNN
     latent_size = 5    
     obs_size = xsTrain.shape[-1]
-    #update_mlp_shape = (5, 5, 5)#@param
-    update_mlp_shape = (3,3)  
-    #choice_mlp_shape = (5, 5, 5)  #@param 
+    update_mlp_shape = (n_features, n_features,)  
     choice_mlp_shape = (2,)
-    #update_mlp_shape = (3,3,)  #@param
-    #Number of hidden units in each of the two layers of the choice MLP.
-    #choice_mlp_shape = (2,)
 
     def make_disrnn():
         model = disrnn.HkDisRNN(
@@ -124,25 +146,8 @@ def train_network_LOOCV(leave_out_idx):
         loss_fun = 'penalized_categorical',
         penalty_scale=information_penalty,
         n_steps=n_steps,
-        do_plot=True,
+        do_plot=False,
         truncate_seq_length=200,
-    )
-    # Now fit more steps with a penalty, to encourage it to find a simple solution
-    # You can experiment with different values, but colab has been tested with 3000.
-    n_steps = 3000  #@param
-    information_penalty = 1e-3 #@param
-
-    disrnn_params, opt_state, losses = rnn_utils.train_model(
-        model_fun = make_disrnn,
-        dataset = train,
-        optimizer = optimizer,
-        loss_fun = 'penalized_categorical',
-        params=disrnn_params,
-        opt_state=opt_state,
-        penalty_scale=information_penalty,
-        n_steps=n_steps,
-        truncate_seq_length=200,
-        do_plot=True,
     )
 
     def compute_log_likelihood(dataset, model_fun, params):
@@ -161,7 +166,7 @@ def train_network_LOOCV(leave_out_idx):
         model_outputs, model_states = rnn_utils.eval_model(model_fun, params, xs)
 
         # Computes the logarithm of the softmax function, which rescales elements to the range [-infinity,0)
-        predicted_log_choice_probabilities = np.array(jax.nn.log_softmax(model_outputs[:, :, :-1])) # model_outputs[:,:,-1] is full of nans!
+        predicted_log_choice_probabilities = np.array(jax.nn.log_softmax(model_outputs[:, :, :-1])) # model_outputs[:,:,-1] is nan!
 
         log_likelihood = 0
         n = 0  # Total number of trials across sessions.
@@ -176,15 +181,19 @@ def train_network_LOOCV(leave_out_idx):
 
         print(f'Normalized Likelihood: {100 * normalized_likelihood:.1f}%')
 
-        return normalized_likelihood
+        return normalized_likelihood, model_outputs
             
     print(f'Normalized Likelihoods for Disentangled RNN, subject: {leave_out_idx+1}')
     print('Training Dataset')
-    training_likelihood = compute_log_likelihood(
-    train, make_disrnn_eval, disrnn_params)
+    training_likelihood, training_output = compute_log_likelihood(train, make_disrnn_eval, disrnn_params)
     print('Held-Out Dataset')
-    testing_likelihood = compute_log_likelihood(
-    test, make_disrnn_eval, disrnn_params)
+    testing_likelihood, test_output = compute_log_likelihood(test, make_disrnn_eval, disrnn_params)
+    
+    # Save the model outputs
+    if not os.path.exists('outputs_LOOCV'):
+        os.makedirs('outputs_LOOCV')
+    with open(f'outputs_LOOCV/outputs_train_{leave_out_idx+1}.pkl', 'wb') as f:
+        pickle.dump(training_output, f)
 
     return training_likelihood, testing_likelihood
 
